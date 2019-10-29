6990b18d372ae3e6fa4947f35576b8199f80271b
fourlexboehm <skeevishlysimple@autistici.org>
2019-10-29 16:01:54 +0000

    Create masterpassword.sh

---
+++ masterpassword.sh
@@ -0,0 +1,12 @@
+
+echo "Enter your full legal name"
+read -p 'Full Name: ' full_name
+
+echo "Enter the primary domain of the account you wish to login to"
+read -sp 'Master Password: ' master_password
+
+echo "Select the Service you wish to obtain your password for"
+select domain in google.com reddit.com soundcloud.com commbank.com.au github.com ebay.com mastodon.social paypal.com systemli.org facebook.com autistici.org factorio.com gog.com riseup.net
+  do
+    echo $master_password | argon2 "$full_name$domain" -l 12 -r -t 100 -p 4 -m 14
+done
